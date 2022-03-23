9713590a7bdd52ff4c9ba08b821f042348ebf000
Donna Adamo <donnastefania.adamo@mercadolibre.com>
2022-03-23 18:22:39 -0300

    form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import Expenses from "./components/Expenses";
+import React from 'react';
 
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense';
+
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -25,7 +28,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses data={expenses}/>
     </div>
   );
